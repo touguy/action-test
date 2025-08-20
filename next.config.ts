5d6c72b54f1af67e513100241959dbd309f4def8
touguy <touguy@korea.com>
2025-08-20 15:13:21 +0900

    user url mod

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: "/user",	// base suburl 지정
   output: "standalone",  // standalone 빌드 활성화
   eslint: {
     ignoreDuringBuilds: true, // 빌드 시 ESLint 검사 무시
